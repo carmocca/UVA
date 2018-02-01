f491564ef73636eaac7b4c40f9c9c714539401ca
Kevin <kezarszy@inf.upv.es>
2018-02-01 15:36:13 +0100

    Minor changes

--- N10189/test.py
+++ N10189/test.py
@@ -6,8 +6,8 @@ dir = os.path.dirname(__file__)
 
 class Tests(unittest.TestCase):
 	def text(self):
-		with open(dir + '/input', 'r') as input_file, \
-				open(dir + '/output', 'r') as output_file:
+		with open(dir + 'samples/input', 'r') as input_file, \
+				open(dir + 'samples/output', 'r') as output_file:
 			self.assertEqual(main(input_file), output_file)
 
 if __name__ == '__main__':
