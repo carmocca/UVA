f95e0ab3331a198da9ba0bf5c6c6ccbf44a20018
Kevin <kezarszy@inf.upv.es>
2018-02-06 17:36:32 +0100

    Add test file and samples | N10044

---
+++ N10044/test.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+from main import *
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+class Tests(unittest.TestCase):
+
+    def test_1(self):
+        with open(dir + '/samples/input1.txt', 'r') as input_file, \
+             open(dir + '/samples/output1.txt', 'r') as output_file:
+            self.assertEqual(main(input_file), output_file.readlines())
+
+if __name__ == '__main__':
+    unittest.main()
