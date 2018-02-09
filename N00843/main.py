ba0b4b5d6a3a3022e3cfbfa8872a215dbdaf88df
carmocca <carlossmocholi@gmail.com>
2018-02-09 04:04:31 +0100

    Use backtracking, still not solved

--- N00843/main.py
+++ N00843/main.py
@@ -1,55 +1,62 @@
 import sys
 import collections
-import re
-import pprint
 
 
-def solve(dictionary, line):
-    words_of_len = collections.defaultdict(list)
-    possible_decodings = collections.defaultdict(set)
-    found_decodings = {}
+def can_match(word, encoded, decodings):
+    return len(word) == len(encoded) and \
+           all(decodings[e] == w for w, e in zip(word, encoded) if decodings[e])
 
-    '''
-    Dict with keys as word lengths and a list of words in
-    dictionary of that length as values
-    '''
-    for word in dictionary:
-        words_of_len[len(word)].append(word)
 
-    for encoded_word in line.split():
-        words = words_of_len[len(encoded_word)]
-        for i, char in enumerate(encoded_word):
-            if len(words) == 1:  # Only one possible decoding for current char
-                found_decodings[char] = set(words[0][i])
-                # TODO: Remove decoded word?
-            else:
-                for w in words: # Add all possible decodings to that char
-                    possible_decodings[char].add(w[i])
+def new_decodings(word, encoded, decodings_input):
+    decodings = decodings_input.copy()
+    for w, e in zip(word, encoded):
+        if decodings[e] == w:
+            continue
+        if decodings[e] is not None or w in decodings.values():
+            return None
+        decodings[e] = w
+    return decodings
 
-    decoded_chars = set.union(*found_decodings.values())
-    decoded_chars_with_repetitions = [x for l in [list(v) for v in found_decodings.values()] for x in l] # Flatten
-    if len(decoded_chars_with_repetitions) != len(decoded_chars):
-        # There are conflicts with decoded chars thus no solution
-        return re.sub(r'\S', '*', line)
 
-    # Remove already decoded char from possible decodings
-    for encoded_char in possible_decodings:
-        possible_decodings[encoded_char].difference_update(decoded_chars)
+def backtracking(dictionary, encoded_words, decodings_input):
+    encoded = encoded_words[0]
+    for word in dictionary[len(encoded)]:
+        match = can_match(word, encoded, decodings_input)
+        decodings = new_decodings(word, encoded, decodings_input)
+        if not match or not decodings:
+            continue
+        if len(encoded_words) == 1:  # End
+            return [word]
+        sol = backtracking(dictionary, encoded_words[1:], decodings)
+        if sol:
+            return [word] + sol
+    return None
 
-    # Add found decodings of encoded chars
-    possible_decodings.update(found_decodings)
 
-    pprint.pprint(dict(possible_decodings))
-    pprint.pprint(dict(found_decodings))
-    return 'TODO'
+def solve(dictionary, encoded_words):
+    characters = [c for l in [list(e) for e in encoded_words] for c in l]
+    decodings = {ch: None for ch in set(characters)}
+    res = backtracking(dictionary, encoded_words, decodings)
+    if not res:
+        res = ['*' * len(e) for e in encoded_words]
+    return ' '.join(res)
 
 
 def main(file):
     res = []
     n = int(file.readline())
-    dictionary = [file.readline().rstrip() for _ in range(n)]
+    '''
+    Dict with keys as word lengths and a list of words in
+    dictionary of that length as values
+    '''
+    dictionary = collections.defaultdict(list)
+    for _ in range(n):
+        word = file.readline().rstrip()
+        dictionary[len(word)].append(word)
+
     for line in file:
-        res.append(solve(dictionary, line.rstrip()) + '\n')
+        encoded_words = line.rstrip().split()
+        res.append(solve(dictionary, encoded_words) + '\n')
     return res
 
 
