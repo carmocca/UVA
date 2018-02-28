d789e569a9f81a2ddeac3af9d806d12e48c31dda
carmocca <carlossmocholi@gmail.com>
2018-02-28 20:59:02 +0100

    Fix solution output

--- N00861/test.py
+++ N00861/test.py
@@ -11,11 +11,6 @@ class Tests(unittest.TestCase):
              open(dir + '/samples/output1.txt', 'r') as output_file:
             self.assertEqual(main(input_file), output_file.readlines())
 
-    def test_2(self):
-        with open(dir + '/samples/input2.txt', 'r') as input_file, \
-             open(dir + '/samples/output2.txt', 'r') as output_file:
-            self.assertEqual(main(input_file), output_file.readlines())
-
 
 if __name__ == '__main__':
     unittest.main()
