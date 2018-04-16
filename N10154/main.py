fd474047269a44fcd4058bf8c2734eeb330245bd
carmocca <carlossmocholi@gmail.com>
2018-04-16 18:55:31 +0200

    Solve Weights and Measures

--- N10154/main.py
+++ N10154/main.py
@@ -7,20 +7,19 @@ Turtle = collections.namedtuple('Turtle', ['weight', 'strength'])
 
 def solve(turtles):
     n = len(turtles)
-    dp = [1e99] * n
+    dp = [float('inf')] * n
     max_, dp[0] = 0, 0
-    for turtle in sorted(turtles):
-        for i in range(n - 1, -1, -1):
-            if dp[i] + turtle.weight <= turtle.strength \
-               and dp[i] + turtle.weight < dp[i + 1]:
-                dp[i + 1] = dp[i] + turtle.weight
+
+    for weight, strength in sorted(turtles, key=lambda t: t.strength):
+        for i in range(max_, -1, -1):
+            if strength > dp[i] + weight < dp[i + 1]:
+                dp[i + 1] = dp[i] + weight
                 max_ = max(max_, i + 1)
     return max_
 
 
 def main(file):
-    turtles = [Turtle(*(int(x) for x in line.split()))
-               for line in file]
+    turtles = [Turtle(*map(int, line.split())) for line in file]
     return solve(turtles)
 
 
